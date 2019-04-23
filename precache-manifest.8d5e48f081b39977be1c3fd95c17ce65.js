34d19a0f4062bc51f4a46e0ce03f59176e89baec
Matthew Elliott <mse951993@gmail.com>
2019-04-23 14:53:24 -0700

    Updates

---
+++ precache-manifest.8d5e48f081b39977be1c3fd95c17ce65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f56ef2045888dc8dfd1",
+    "url": "/react-clicker-homework/static/css/main.daa6eec1.chunk.css"
+  },
+  {
+    "revision": "4f56ef2045888dc8dfd1",
+    "url": "/react-clicker-homework/static/js/main.8c2ffe4e.chunk.js"
+  },
+  {
+    "revision": "5a1af82c416778be6ba1",
+    "url": "/react-clicker-homework/static/js/runtime~main.b2202ab4.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-clicker-homework/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "cde4f713dac447a5c1972a148cc5bac4",
+    "url": "/react-clicker-homework/index.html"
+  }
+];
